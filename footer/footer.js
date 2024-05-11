b15c269c53371b1aba4521af9f741fa961ee6366
bemmoralesmora <159475376+bemmoralesmora@users.noreply.github.com>
2024-05-11 13:38:01 -0600

    Add files via upload

---
+++ footer/footer.js
@@ -0,0 +1,22 @@
+let dom_footer = document.querySelector(".footer")
+
+dom_footer.innerHTML= `
+<div class="laboratorio">
+<h1>Laboratorio 1</h1>
+<br>
+<h2>Programado y creado por: Samuel Sarazua y Brian Morales</h2>
+</div>
+<div class="suscribirse">
+<h4>Suscribirse para mas informacion</h4>
+<div class="cuadros">
+<div class="cuadro1_0">Suscribete</div>
+<div class="cuadro2_0"></div>
+</div>
+</div>
+<div class="redes-sociales">
+<h2>Redes Sociales</h2>
+    <div>Facebook</div>
+    <div>Twitter</div>
+    <div>Instagram</div>
+</div>
+`;
---
+++ header/header.js
@@ -0,0 +1,14 @@
+let dom_header = document.querySelector(".header")
+
+dom_header.innerHTML = `
+<div class="nombre">Laboratorio1</div>
+<div class="opciones">
+    <a href="index.html" class="lista2">Home</a>
+    <div class="lista1">About us</div>
+    <div class="lista1">Services</div>
+    <div class="lista1">Products</div>
+    <div class="lista1">Pricing</div>
+    <div class="lista1">Contact us</div>
+</div>
+<div class="boton1">Get started</div>
+`;
